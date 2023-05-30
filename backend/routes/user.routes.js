8f2a32de215d3f29fb2cc7d6b0f33fdd4af4eb9f
Deepak Dhankhar <111503473+Deepak1095@users.noreply.github.com>
2023-05-30 22:30:52 +0530

    user login with token

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,12 @@
 const express=require("express")
 const bcrypt=require("bcrypt")
+const jwt=require("jsonwebtoken")
 const { UserModel } = require("../model/user.model")
 const userRouter=express.Router()
 
 userRouter.post("/register",async(req,res)=>{
     const {name,email,password,age}=req.body
-    
+
     try{
         bcrypt.hash(password,5,async(err,hash)=>{
             if(err){
@@ -18,6 +19,32 @@ userRouter.post("/register",async(req,res)=>{
             }
         })
        
+    }
+    catch(err){
+        res.status(400).json({err:err.message})
+    }
+})
+
+userRouter.post("/login",async(req,res)=>{
+    const {email,password}=req.body
+    try{
+        const user=await UserModel.findOne({email})
+        if(user){
+            bcrypt.compare(password,user.password,(err,result)=>{
+                if(result){
+                    const token=jwt.sign({data:"Backend"},"india", { expiresIn: '1h' })
+                    console.log(token)
+                    res.status(200).json({msg:"user is login ",token:token})
+                }
+                else{
+                    res.status(200).json({err:err.message})
+                }
+            })
+        }
+        else{
+            res.status(200).json({msg:"user not found please register"})
+        }
+
     }
     catch(err){
         res.status(400).json({err:err.message})
