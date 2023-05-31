0f55edec58db8fd6618ba28306dcfd5253ef5d77
Deepak Dhankhar <111503473+Deepak1095@users.noreply.github.com>
2023-05-31 21:43:51 +0530

    Add Delete CRUD endpoints for book resource

--- backend/routes/books.routes.js
+++ backend/routes/books.routes.js
@@ -1,17 +1,29 @@
 const express=require("express")
-const { UserModel } = require("../model/user.model")
-const bookRoutes=express.Router()
+const { BookModel } = require("../model/books.model")
+const bookRouter=express.Router()
 
-bookRoutes.get("/",async(req,res)=>{
+bookRouter.get("/",async(req,res)=>{
     try{
- const Books=await UserModel.find()
- res.status(400).json({Books:Books})
+ const Books=await BookModel.find()
+ res.status(200).json({Books:Books})
 }
 catch(err){
-res.status(400).json({msg:""})
+res.status(400).json({err:err.message})
 }
 })
 
+bookRouter.delete("/delete/:id",async(req,res)=>{
+    const {id}=req.params
+    try{
+        
+        const DeletedBook=await BookModel.findByIdAndDelete({_id:id})
+        res.status(200).json({msg:"This book is deleted","Deleted book":DeletedBook})
+    }
+    catch(err){
+        res.status(400).json({err:err.message})
+    }
+})
+
 module.exports={
-    bookRoutes
+    bookRouter
 }
