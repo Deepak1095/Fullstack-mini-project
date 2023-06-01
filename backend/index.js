94e63d8ca58dcd9d40c8594faaf19a59a95120b0
Deepak Dhankhar <111503473+Deepak1095@users.noreply.github.com>
2023-06-01 22:37:02 +0530

    get userRouter and BookRouter

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 const express=require("express")
 const { connection } = require("./config/db")
+const { userRouter } = require("./routes/user.routes")
+const { bookRouter } = require("./routes/books.routes")
+
 require("dotenv").config()
 const app=express()
 app.use(express.json())
-
+app.use("/user",userRouter)
+app.user("/books",bookRouter)
 app.listen(process.env.PORT,async(req,res)=>{
 try{
     await connection
