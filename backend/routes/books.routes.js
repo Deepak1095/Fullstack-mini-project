8005604cedbd058253f212931f50593f2ef519bc
Deepak Dhankhar <111503473+Deepak1095@users.noreply.github.com>
2023-06-01 22:49:38 +0530

    book add and update

--- backend/routes/books.routes.js
+++ backend/routes/books.routes.js
@@ -24,6 +24,33 @@ bookRouter.delete("/delete/:id",async(req,res)=>{
     }
 })
 
+bookRouter.post("/addBook",async(req,res)=>{
+    try{
+        const newBook=new BookModel(req.body)
+        await newBook.save()
+        res.status(200).json({msg:"new book is added"})
+    }catch(err){
+        res.status(400).json({err:err.message})
+    }
+})
+
+bookRouter.post("/update/:id",async(req,res)=>{
+    const {id}=req.params
+    try{
+       const book= await BookModel.findByIdAndUpdate({_id:id},req.body)
+       if(book){
+        res.status(200).json({msg:"book is update"})
+       }
+       else{
+        res.status(200).json({msg:"book not present"})
+       }
+       
+    }catch(err){
+        res.status(400).json({err:err.message})
+    }
+})
+
+
 module.exports={
     bookRouter
 }
