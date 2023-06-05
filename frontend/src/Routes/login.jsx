2f833e70568990a0384eb8677f70f026a7670adf
Deepak Dhankhar <111503473+Deepak1095@users.noreply.github.com>
2023-06-05 23:12:43 +0530

    connect login into backend and save token and rtoken in localstorage

--- frontend/src/Routes/login.jsx
+++ frontend/src/Routes/login.jsx
@@ -6,6 +6,20 @@ const Login = () => {
     const [password, setPassword] = useState("")
     const handleBtn = () => {
         const payload = { email, password }
+        fetch("http://localhost:9090/user/login",{
+            method:"POST",
+            headers:{
+                "Content-type":"application/json"
+            },
+            body:JSON.stringify(payload)
+        })
+        .then((res)=>res.json())
+        .then((res)=>{
+            localStorage.setItem("token",res.token)
+            localStorage.setItem("rtoken",res.rtoken)
+            console.log(res)
+        })
+        .catch((err)=>console.log(err))
     }
     return (
         <Box>
