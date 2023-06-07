72380b45e19311d06bb1ca5f56a5e79e6547349a
Deepak Dhankhar <111503473+Deepak1095@users.noreply.github.com>
2023-06-07 23:57:46 +0530

    login css

--- frontend/src/Routes/login.jsx
+++ frontend/src/Routes/login.jsx
@@ -1,4 +1,4 @@
-const { Box, Input, Button } = require("@chakra-ui/react")
+const { Box, Input, Button, FormControl, FormLabel } = require("@chakra-ui/react")
 const { useState } = require("react")
 
 const Login = () => {
@@ -6,26 +6,31 @@ const Login = () => {
     const [password, setPassword] = useState("")
     const handleBtn = () => {
         const payload = { email, password }
-        fetch("https://impossible-earrings-clam.cyclic.app/user/login",{
-            method:"POST",
-            headers:{
-                "Content-type":"application/json"
+        fetch("https://impossible-earrings-clam.cyclic.app/user/login", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
             },
-            body:JSON.stringify(payload)
+            body: JSON.stringify(payload)
         })
-        .then((res)=>res.json())
-        .then((res)=>{
-            localStorage.setItem("token",res.token)
-            localStorage.setItem("rtoken",res.rtoken)
-            console.log(res)
-        })
-        .catch((err)=>console.log(err))
+            .then((res) => res.json())
+            .then((res) => {
+                localStorage.setItem("token", res.token)
+                localStorage.setItem("rtoken", res.rtoken)
+                console.log(res)
+            })
+            .catch((err) => console.log(err))
     }
     return (
-        <Box>
-            <Input type="email" name="email" placeholder="Enter your email" onChange={(e) => setEmail(e.target.value)} />
-            <Input type="password" name="password" placeholder="Enter your password" onChange={(e) => setPassword(e.target.value)} />
-            <Button onClick={handleBtn}>Login</Button>
+        <Box width="300px" margin={"auto"} marginTop={"150px"} border={"1px solid black"} padding="20px">
+            <FormControl>
+                <FormLabel>Email address</FormLabel>
+                <Input width={"100%"} height={"25px"} marginBottom={"10px"} type="email" placeholder="Enter your email" onChange={(e) => setEmail(e.target.value)} />
+                <FormLabel>Email address</FormLabel>
+                <Input width="100%" height={"25px"} marginBottom={"10px"} type="password" placeholder="Enter your password" onChange={(e) => setPassword(e.target.value)} />
+            </FormControl>
+
+            <Button size='md'  height='30px'  width="50%" border='2px solid green.500' onClick={handleBtn}>Login</Button>
         </Box>
     )
 }
